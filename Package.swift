1cf9965cb9d7856e8901bda989cf2241fe2a710f
cache <cache0928@hotmail.com>
2020-07-09 16:53:04 +0800

    删除可执行Target

--- Package.swift
+++ Package.swift
@@ -22,9 +22,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Runner",
-            dependencies: ["Razor"]),
-        .target(
             name: "Razor",
             dependencies: ["Alamofire"]),
         .testTarget(
